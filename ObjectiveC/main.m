5c341c24c9c2d9916e80b3c7c099b455d079f14a
F839-010 M <mf839-010@70-12-244-212.pools.spcsdns.net>
2016-05-23 17:19:05 +0900

    Initial Commit

---
+++ ObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveC
+//
+//  Created by MF839-010 on 2016. 5. 23..
+//  Copyright © 2016년 Sample. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
